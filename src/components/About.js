3a1536022dbe3647333daa6fa5aec72e034bfb87
William Lorey <will@williamlorey.com>
2020-02-02 14:36:22 -0800

    Update photos & card titles

--- src/components/About.js
+++ src/components/About.js
@@ -8,22 +8,29 @@ class About extends Component {
       <div className="About">
         <div className="header-text">Hermann Homestead</div>
         <Card
-          image="goats.jpg"
-          heading="Something about goats"
-          text="Pet our goats. Let them crawl on you. Heck, crawl on them too. Yoga!"
+          image="becky-and-jim.png"
+          heading="Meet the Family"
+          text=""
         />
         <Card
-          image="cabin.jpg"
+          image="yoga.png"
+          heading="Yoga"
+          text=""
+        />
+        <Card
+          image="cabin.png"
           heading="Cabins"
-          text="Reserve your very own rustic cabin for a romantic weekend. Or if you just 
-          want to sh*t in the woods."
+          text=""
         />
         <Card
-          image="wurst.jpg"
-          heading="Hit the town"
-          text="Explore our town - scenic Hermann, MO. Sample the local cuisine, spend an evening at
-          one of the area's wineries, or even meet the Wurstmeister, Hermann's premier sausage-based
-          celebrity."
+          image="lumber.png"
+          heading="Lumber"
+          text=""
+        />
+        <Card
+          image="gallery.png"
+          heading="Gallery"
+          text=""
         />
       </div>
     );
