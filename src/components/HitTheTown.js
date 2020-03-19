e63447bb3bc5cf71e5280dc0347580f5e2de4c69
rlorey <62402303+rlorey@users.noreply.github.com>
2020-03-19 16:32:37 -0500

    Update HitTheTown.js

--- src/components/HitTheTown.js
+++ src/components/HitTheTown.js
@@ -10,6 +10,8 @@ class HitTheTown extends Component {
           What to do?
           <ul>
             <li>Restaurants</li>
+<li>El Ranchito Mexican Restaurant - https://www.facebook.com/Elranchitohermann701/?rf=111601885543467 This is one of our favorites.  We usually go at least once a week.  We love their salsa!  All the food comes highly recommended.  Jim’s favorite is the burrito grande and mine is more complicated.  I eat gluten free and plant based and I am pretty high maintenance when we go out to eat in terms of the modifications that I ask for with my meal.  The servers and cooks at El Ranchito are absolutely wonderful about making any changes that you want.  My favorite is the Veggie #2 with lettuce, no cheese sauce, and corn tortillas instead of flour.  Then I put a whole dish of salsa on top.  Yum!  Their margaritas are fantastic and the servers are incredibly friendly and accomodating! </li>
+
             <li>Antiquing and Shopping in Hermann</li>
             <li>Wineries</li>
             <li>Distilleries</li>
